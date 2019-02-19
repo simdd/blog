57e417f140ef2863a1197d08b15d50aca39289cc
SIMDD <dev.simdd@gmail.com>
2019-02-19 23:16:45 +0800

    feat: 🎸 add object.create mock

---
+++ this/create.js
@@ -0,0 +1,12 @@
+Object.prototype.mockCreate = function(Parent) {
+  let obj = {};
+  obj.__proto__ = Parent;
+  return obj;
+};
+
+var Parent = {
+  name: "this is obj",
+  age: 1
+};
+
+let obj = Object.mockCreate(Parent);
