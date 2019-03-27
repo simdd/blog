eefba0ad480adedcb76ec873e5cd9e46e0dc7a29
SIMDD <dev.simdd@gmail.com>
2019-03-28 01:05:05 +0800

    feat: 🎸 添加thunk

---
+++ thunk/thunk.js
@@ -0,0 +1,25 @@
+var readFileThunk = function(fileName) {
+  return function(callback) {
+    return fs.readFile(fileName, callback);
+  };
+};
+
+var g = function*() {
+  var f1 = yield readFileThunk("fileA");
+  var f2 = yield readFileThunk("fileB");
+  var fn = yield readFileThunk("fileN");
+};
+
+function run(fn) {
+  var gen = fn();
+
+  function next(err, data) {
+    var result = gen.next(data);
+    if (result.done) return;
+    result.value(next);
+  }
+
+  next();
+}
+
+run(g);
